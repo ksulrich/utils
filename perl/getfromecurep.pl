5e5346b7223dce2a456ecd247ebf4b52edef8c03
Klaus Ulrich <klulrich@oc8848372212.ibm.com>
2011-09-09 14:21:09 +0200

    adapted for RHEL and AE5

--- perl/getfromecurep.pl
+++ perl/getfromecurep.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl -w
 
-my $path = shift @ARGV;
+my $pmr = shift @ARGV;
+my $path = $pmr;
+my $pmr_dir = "/local/PMR";
 
-my $pmr_dir = "/PMR";
-
-if ($path =~ /^[\d]+[\d,A-Z]+$/) {
-  @path = split(/ */, $path);
-  $first = $path[0];
-  $second = $path[1];
+if ($pmr =~ /^[\d]+[\d,A-Z]+$/) {
+  @pmr = split(/ */, $pmr);
+  $first = $pmr[0];
+  $second = $pmr[1];
   $path = "/ecurep/pmr/$first/$second/$path"
 }
 
@@ -20,7 +20,13 @@ system($cmd) == 0 or
   die "Cant get $path from ecurep server\n";
 
 # cd to this directory
-$path =~ s|^.*/(.*)$|$1|;
-chdir $path;
-system("explorer.exe .");
-exec "/bin/bash";
+$dir =~ s|^.*/(.*)$|$1|;
+chdir $dir;
+#system("explorer.exe .");
+system("emacs -fn 8x13 &");
+#system("xterm &");
+system("gnome-terminal &");
+
+my $host = "ecurep.mainz.de.ibm.com";
+#print "firefox https://$host/ae5/login/login.html#id=$pmr&path=$path &\n";
+system("firefox https://$host/ae5/login/login.html#id=$pmr&path=$path &")
