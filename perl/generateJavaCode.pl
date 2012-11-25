f83cf82bf1e3c3b8d38935f7593b070f94581f13
Klaus Ulrich <klaus.ulrich@web.de>
2012-11-25 18:56:48 +0100

    skip to next class file if java file already exist

--- perl/generateJavaCode.pl
+++ perl/generateJavaCode.pl
@@ -8,16 +8,17 @@ open(FIND, "find $dir -name \"*.class\" -print |") or
 $home = `pwd`;
 chomp($home);
 while ($file = <FIND>) {
+    chdir $home;
     chomp($file);
-    print "FILE: '$file'\n";
+#    print "FILE: '$file'\n";
     $file =~ m|^(.*)/(.*)\.class$|;
     $dir = $1;
     $name = $2;
 #    print "DIR: $dir, NAME: $name\n";
-    chdir $dir;
+    chdir $dir or warn "Can not chdir to $dir. I am in \"", `pwd`, "\": $!";
 #    print "IN: ", `pwd`;
-    print "Call jad -o -sjava $name.class\n";
-    system("jad", "-o", "-sjava", "$name.class");
-    chdir $home;
+    next if -e "$name.java";
+#    print "Call jad -o -sjava $name.class\n";
+    system("jad", "-lnc", "-o", "-sjava", "$name.class");
 #    print "BACK: ", `pwd`;
 }
