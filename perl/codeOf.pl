2a3467d28268c33d7a8a84041f5eab2102011b53
Klaus Ulrich <klaus.ulrich@web.de>
2013-03-02 11:54:12 +0100

    Works with full class name on several plugins

--- perl/codeOf.pl
+++ perl/codeOf.pl
@@ -2,7 +2,7 @@
 
 require "getopts.pl";
 
-$debug = 1;
+$debug = 0;
 
 $tmp = "/tmp/xxx";
 
@@ -19,25 +19,45 @@ $class = norm_class($class);
 if (fileExists("$class.class")) {
     callEmacs("$class.java");
 } else {
-    print "DEBUG: grepbpm $class\n";
+    print "DEBUG: grepbpm $class\n" if ($debug);
     open(GREP, "grepbpm $class |") or die "Cant call grepbpm $class: $!\n";
+    my @plugins = undef;
     while ($line = <GREP>) {
+	next if ($line =~ m/^\s*$/);
 	chomp($line);
 	print ("DEBUG: LINE$line\n") if ($debug);
 	if ($line =~ m/^\s*(.*): (.*)$/) {
 	    $plugin = $1;
 	    $class = $2;
+	    push(@plugins, $plugin);
 	    print "DEBUG: PLUGIN=$plugin, CLASS=$class\n" if ($debug);
-	    if (usePlugin($plugin)) {
-		unless (fileExists($class)) {
-		    explode($plugin);
-		    generateJavaCode();
-		}
-		callEmacs($class);
-	    }
 	}
     }
+    my $len = scalar(@plugins);
+    if ($len < 2) {
+	print "Code not found\n";
+	exit(0);
+    } 
+    if ($len == 2) {
+	$plugin = @plugins[1];
+	print "DEBUG: len==2: Use $plugin\n" if ($debug);
+    } else {
+	my $i = 0;
+	foreach $plugin (@plugins) {
+	    print "$i: $plugin\n";
+	    $i++;
+	}
+	print "What plugin? ... ";
+	$ans = <>;
+	$ans =~ s/(\d+).*/\1/;
+	print "DEBUG: Use $ans\n" if ($debug);
+	$plugin = $plugins[$ans];
+    }
+    explode($plugin);
+    generateJavaCode();
+    callEmacs($class);
 }
+
 sub usePlugin {
     my ($plugin) = @_;
     print "Use '$plugin' ? (y/n) ... ";
@@ -50,13 +70,14 @@ sub usePlugin {
 
 sub explode {
     my ($plugin) = @_;
-    print "DEBUG(explode): explode.pl $plugin\n" if ($debug);
-    system("explode.pl", $plugin)
+    $plugin =~ s/ /\\ /g;
+    print "DEBUG(explode): cd /tmp/xxx; jar xf $plugin\n" if ($debug);
+    system("mkdir -p /tmp/xxx; cd /tmp/xxx; jar xf $plugin");
 }
 
 sub generateJavaCode {
     print "DEBUG(generateJavaCode): generateJavaCode.pl $tmp\n" if ($debug);
-    system("generateJavaCode.pl", $tmp);
+    system("generateJavaCode.pl $tmp 2>/dev/null");
 }
 
 sub callEmacs {
