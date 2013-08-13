68e5080f87f7f139b24b645af9b0cc7f34930ffc
Klaus Ulrich <klaus.ulrich@web.de>
2013-08-13 14:23:23 +0200

    find command to generate a list of "<retain_id>" "<user>" duples

---
+++ sh/search_retain_ids.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Needs to run on a CCWin installation server
+# 
+# Creates a list of "<retain_id>" "<user>" 
+
+find /c/CCWin -name "owner.dat" -exec grep -v // {} \; | grep -v ^\# | sort | grep -v \"\" | grep -v "      "
